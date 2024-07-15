d4d7a3e8d51ce93789a443fff2ce68c4dc763e45
diegohp <diegohp.dev@gmail.com>
2024-07-15 00:09:22 -0600

    Add validate UserData record using bean validation dependency

--- backend/src/main/java/com/diegohrp/backend/controllers/UserController.java
+++ backend/src/main/java/com/diegohrp/backend/controllers/UserController.java
@@ -10,8 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.http.HttpResponse;
-import java.util.Optional;
+
 
 @RestController
 @RequestMapping("/users")
--- backend/src/main/java/com/diegohrp/backend/dtos/UserData.java
+++ backend/src/main/java/com/diegohrp/backend/dtos/UserData.java
@@ -1,8 +1,16 @@
 package com.diegohrp.backend.dtos;
 
 import com.diegohrp.backend.entities.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
-public record UserData(String name, String lastName, String username, String email, String password) {
+
+public record UserData(@NotBlank String name,
+                       @NotBlank String lastName,
+                       @NotBlank String username,
+                       @NotBlank @Email String email,
+                       @NotBlank @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[A-Za-z\\d]{8,}$") String password) {
 
     public UserData(User user) {
         this(user.getName(), user.getLastName(), user.getUsername(), user.getEmail(), user.getPassword());
